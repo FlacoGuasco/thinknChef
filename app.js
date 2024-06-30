b73cfdacfe91b8a7baadf37d282caa90373581d7
Cesar Guasco <dibcad2074@gmail.com>
2024-06-30 13:35:13 -0300

    base_original_proyecto

---
+++ app.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+
+//Llamamos a la función que contiene un objeto "express"
+const express = require('express');
+
+/* const menuRouter = require('./src/routes/routes-menu');
+const contactoRouter = require('./src/routes/routes-contacto');
+const nosotrosRouter = require('./src/routes/routes-nosotros');
+const ayudaRouter = require('./src/routes/routes-ayuda');
+const loginRouter = require('./src/routes/routes.login');
+const indexRouter = require('./src/routes/routes-index');
+const error404Router = require('./src/routes/routes-error404'); */
+
+// Instanciamos la función del objeto express
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Este método sirve para buscar primero en la carpeta 'public', sino, sigue buscando
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Usamos el motor de vistas EJS y le indicamos la carpeta donde estan las vistas a usar
+/* app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'))*/
+
+app.use('/', (req, res) => {
+    res.send('Todo funcionando OK.')
+})
+/*
+app.use('/menu', menuRouter);
+app.use('/contacto', contactoRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/ayuda', ayudaRouter);
+app.use('/login', loginRouter);
+app.use('/index', indexRouter);
+app.use(error404Router); */
+
+// Verificamos que el server este funcionando
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server funcionando en http://${process.env.HOST}:${process.env.PORT}`);
+});
